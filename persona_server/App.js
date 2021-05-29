6c5aabd17a7d2830893057e460c260319248a0e8
Kim Dong Hun <hunkim98@gmail.com>
2021-05-29 14:13:41 +0900

    testing heroku functionality

--- persona_server/App.js
+++ persona_server/App.js
@@ -5,10 +5,17 @@ const app = express();
 const port = process.env.PORT || 5000;
 const Datastore = require("nedb");
 const fetch = require("node-fetch");
+var path = require("path");
 
 const database = new Datastore("database.db");
 database.loadDatabase();
 app.use(express.json({ limit: "1mb" }));
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.get("/gatherData", (req, res) => {
   database.find({}, (err, docs) => {
